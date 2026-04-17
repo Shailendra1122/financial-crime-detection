5e460e89721596a2567638a81daeee0a38d99f8c
Shailendra Pratap Singh <Shailendraprbns@gmail.com>
2026-04-17 19:45:21 +0530

    Setup backend with MySQL and basic homepage

---
+++ backend/financial-crime-detection/src/main/java/com/financial/crime/detection/FinancialCrimeDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.financial.crime.detection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialCrimeDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialCrimeDetectionApplication.class, args);
+	}
+
+}
---
+++ backend/financial-crime-detection/src/test/java/com/financial/crime/detection/FinancialCrimeDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financial.crime.detection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialCrimeDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
