2b00da49c0b360349eaf16c252a4fb5ee20b5c94
Shailendra Pratap Singh <Shailendraprbns@gmail.com>
2026-04-20 19:08:38 +0530

    UI change

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -17,7 +17,7 @@ API.interceptors.request.use((config) => {
 API.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response && error.response.status === 401) {
+    if (error.response && (error.response.status === 401 || error.response.status === 403)) {
       localStorage.removeItem("token");
       localStorage.removeItem("user");
       window.location.href = "/login";
